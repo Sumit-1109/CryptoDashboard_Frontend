9195000463a919beed0c09510b8dedfa88ad24cd
Sumitkumar1109 <sumitkumar2000sep@gmail.com>
2025-04-19 10:44:50 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// Vite configuration
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', 
   plugins: [react()],
-  build: {
-    outDir: 'dist', 
-    rollupOptions: {
-      input: '/index.html'
-    }
-  }
-})
+});
